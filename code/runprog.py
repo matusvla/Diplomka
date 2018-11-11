712ed6adf84c0800524247013ed1878abda7d23d
Vladislav Matus <Matus@Vladislavs-MacBook-Pro.local>
2018-11-11 23:00:20 +0100

    Extended python script for poisson matrix

--- code/runprog.py
+++ code/runprog.py
@@ -6,7 +6,7 @@ import os
 import sys
 from subprocess import call, Popen, PIPE
 
-MATRIX_PATH = "testmatrices"
+MATRIX_PATH = "matrices"
 OUTPUT_FILE = "progoutput.txt"
 DELIMITER = "-------------------------------------------------------"
 
@@ -18,8 +18,15 @@ try:
 except OSError:
     pass
 
-for matrix in os.listdir(MATRIX_PATH):
-    print("Processing " + matrix)
-    command = "./prog -o ./" + MATRIX_PATH + "/" + matrix + " -t >> progoutput.txt"
+# for matrix in sorted(os.listdir(MATRIX_PATH)):
+#     print("Processing " + matrix)
+#     command = "./prog -mt P3 -o ./" + MATRIX_PATH + \
+#         "/" + matrix + " >> progoutput.txt"
+#     child = Popen(command, shell=True, stdout=PIPE)
+#     print(child.communicate()[0] + DELIMITER)
+
+for matrix in range(3, 50):
+    print("Processing P" + str(matrix))
+    command = "./prog -mt P"+str(matrix)+" >> progoutput.txt"
     child = Popen(command, shell=True, stdout=PIPE)
-    print child.communicate()[0] + DELIMITER
+    print(child.communicate()[0] + DELIMITER)

2a4264749e67b73a01a63df90f9355f76ac73f13
Vladislav Matus <matusvla@fjfi.cvut.cz>
2018-11-25 10:44:39 +0100

    Python printing in latex table format

--- code/runprog.py
+++ code/runprog.py
@@ -4,6 +4,7 @@
 
 import os
 import sys
+import re
 from subprocess import call, Popen, PIPE
 
 MATRIX_PATH = "matrices"
@@ -19,20 +20,32 @@ except OSError:
     pass
 
 # for matrix in sorted(os.listdir(MATRIX_PATH)):
-#     print("Processing " + matrix)
-#     command = "./prog -o ./" + MATRIX_PATH + \
-#         "/" + matrix + " >> progoutput.txt"
+#     # print("Processing " + matrix)
+#     command = "./prog -f ./" + MATRIX_PATH + \
+#         "/" + matrix + \
+#         ""
+#     proc = Popen(command, shell=True, stdout=PIPE)
+#     (out,err) = proc.communicate()
+#     outValues = map(int, re.findall(' \d+', out))
+#     if len(outValues) == 0 :
+#         print(matrix + " \t&\tERROR \t&\tERROR \t&\tERROR \t//")
+#     else:
+#         print(matrix + '\t&\t' + '\t&\t'.join(map(str,outValues)) + "\t//")
+
+for size in range(3, 50):
+    # print("Processing P" + str(matrix))
+    command = "./prog -mt P"+str(size)+ \
+        ""
+    proc = Popen(command, shell=True, stdout=PIPE)
+    (out,err) = proc.communicate()
+    outValues = map(int, re.findall(' \d+', out))
+    if len(outValues) == 0 :
+        print('P'+ size + " \t&\tERROR \t&\tERROR \t&\tERROR \t//")
+    else:
+        print('P'+ str(size) + '\t&\t' + '\t&\t'.join(map(str,outValues)) + "\t//")
+
+# for matrix in range(1, 8):
+#     print("Processing T" + str(matrix))
+#     command = "./prog -mt T"+str(matrix)+" -t >> progoutput.txt"
 #     child = Popen(command, shell=True, stdout=PIPE)
 #     print(child.communicate()[0] + DELIMITER)
-
-# for matrix in range(3, 50):
-#     print("Processing P" + str(matrix))
-#     command = "./prog -mt P"+str(matrix)+" >> progoutput.txt"
-#     child = Popen(command, shell=True, stdout=PIPE)
-#     print(child.communicate()[0] + DELIMITER)
-
-for matrix in range(1, 8):
-    print("Processing T" + str(matrix))
-    command = "./prog -mt T"+str(matrix)+" >> progoutput.txt"
-    child = Popen(command, shell=True, stdout=PIPE)
-    print(child.communicate()[0] + DELIMITER)
